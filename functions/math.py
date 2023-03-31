7c25de5943b68846c978147cc26c5b7d03b19c3d
Angiefiona <angellamumoh@gmail.com>
2023-03-31 22:05:59 +0300

    first commit

---
+++ functions/math.py
@@ -0,0 +1,19 @@
+def add (a,b):
+    answer = a+b
+    return answer
+
+def subtract(m,n):
+    answer = m-n
+    return answer
+
+def divide(y,z):
+    answer = y/z
+    return answer
+
+def multiply(d,f):
+    answer = d*f
+    return answer
+
+def remainder(g,h):
+    answer = g%h
+    return answer
